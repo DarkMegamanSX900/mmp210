0be14c0aee98e3c3c1f5b82a4dda9aa5527e0a00
Kevinstion <kevin.hendricks@aol.com>
2017-04-17 17:25:50 -0400

    Add files via upload
    folder updates.

--- Week5/sketch.js
+++ Week5/sketch.js
@@ -1,22 +1,34 @@
-function setup(){
-    createCanvas(640,360);
+// windows
+function setup() { 
+   createCanvas(640, 480);
+    noStroke();
 }
 
-function draw(){
-    background(220);
+function draw() {
+    background("lightgreen");
     
-    stroke("yellow");
+    var windowWidth = width/6;
+    var windowHeight = height/2; 
     
-    var numColums = 10;
-    
-    var columWidth = width / numColums;
-    var rowHeight = height / numColums;
-    
+    for (var x = windowWidth/4; x < width; x+=windowWidth){
+       for (var y = windowHeight/4; y < height; y+=windowHeight) {  
     
     
-    for (var i = 0; i < numColums; i++) {
-        line(columWidth*i, 0, columWidth*i, height);
-        
-        line(0,rowHeight*i,width,rowHeight*i);
-    }
+    fill("lightblue");
+    rect(x, y, windowWidth/2, windowHeight/2);
+    
+    fill("White");
+    stroke(167, 82, 54);
+           
+           rect(x, y + windowWidth/4, windowHeight/8);
+           rect(x, y + windowHeight/4, windowWidth/4, windowHeight/0);
+           rect(x, y + windowHeight/4, windowWidth/4, windowHeight/8);
+           rect(x, y + windowHeight/8, windowWidth/4, windowHeight/8);
+           rect(x + windowWidth/4, y + windowHeight/8, windowWidth/4, windowHeight/8);
+           rect(x + windowWidth/4, y + windowHeight/8 * 2, windowWidth/4, windowHeight/8);
+           rect(x + windowWidth/4, y + windowHeight/8 * 1, windowWidth/8, windowHeight/8);
+           rect(x + windowWidth/100, y + windowHeight/8 * 1, windowWidth/10, windowHeight/8);
+  }
+ }
 }
+    
